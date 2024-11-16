38eac3610f2ebf9cc20769d2dbbee6072471de46
Matheus Alves <matheusalves@10.0.0.127>
2024-11-16 20:06:02 -0300

    Finalizado o projeto de tarefas em React

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,139 @@
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import { BsTrash, BsBookmarkCheck, BsBookmarkCheckFill } from "react-icons/bs";
+
+const API = "http://localhost:50000";
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [timer, setTimer] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  // Load todo on page load
+  useEffect(() => {
+    const loadData = async () => {
+      setLoading(true);
+
+      const res = await fetch(API + "/todos")
+        .then((res) => res.json())
+        .then((data) => data)
+        .catch((err) => err);
+
+      setLoading(false);
+
+      setTodos(res);
+    };
+
+    loadData();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const todo = {
+      id: todos.length + 1,
+      title,
+      timer,
+      done: false,
+    };
+
+    await fetch(API + "/todos", {
+      method: "POST",
+      body: JSON.stringify(todo),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    setTodos((prevState) => [...prevState, todo]);
+
+    setTitle("");
+    setTimer("");
+  };
+
+  const handleDone = async (todo) => {
+    todo.done = !todo.done;
+
+    const data = await fetch(API + "/todos/" + todo.id, {
+      method: "PUT",
+      body: JSON.stringify(todo),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    setTodos((prevState) =>
+      prevState.map((t) => (t.id === data.id ? (t = data) : t))
+    );
+  };
+
+  const handleDelete = async (id) => {
+    await fetch(API + "/todos/" + id, {
+      method: "DELETE",
+    });
+
+    setTodos((prevState) => prevState.filter((todo) => todo.id !== id));
+  };
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
   return (
-    <div className="app">
+    <div className="App">
+      <div className="todo-header">
+        <h1>React Todo</h1>
+      </div>
+
+      <div className="form-todo">
+        <h2>Create new tasks</h2>
+
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="title">What are you up to next?</label>
+            <input
+              type="text"
+              name="title"
+              placeholder="Task title..."
+              onChange={(e) => setTitle(e.target.value)}
+              value={title}
+              required
+            />
+          </div>
+
+          <div className="form-control">
+            <label htmlFor="timer">And how many hours will it take?</label>
+            <input
+              type="number"
+              placeholder="Ex.: 1, 2, 5, 10..."
+              onChange={(e) => setTimer(e.target.value)}
+              value={timer}
+              required
+            />
+          </div>
+
+          <input type="submit" value="Create task" />
+        </form>
+      </div>
+      <div className="list-todo">
+        <h2>Task list:</h2>
+        {todos.length === 0 && <p>There are no tasks</p>}
+        {todos.map((todo) => (
+          <div className="todo" key={todo.id}>
+            <h3 className={todo.done ? "todo-done" : ""}>{todo.title}</h3>
+            <p>Time needed: {todo.time}h</p>
+            <div className="actions">
+              <span onClick={() => handleDone(todo)}>
+                {!todo.done ? <BsBookmarkCheck /> : <BsBookmarkCheckFill />}
+              </span>
+              <BsTrash onClick={() => handleDelete(todo.id)} />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
